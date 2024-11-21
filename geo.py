427d7ac7d4750f50f0108eae040dfcb72fee0f55
zkhan <apps4av@gmail.com>
2024-11-21 09:40:55 -0500

    database for geoid height and declination

--- geo.py
+++ geo.py
@@ -1,13 +1,17 @@
 import pygeodesy
 from pygeodesy.ellipsoidalKarney import LatLon
 from magnetic_field_calculator import MagneticFieldCalculator
+from tqdm import tqdm
 
 # Create a geo database of declination and geoid height
 
 calculator = MagneticFieldCalculator()
-interpolator = pygeodesy.GeoidKarney("./egm84-30.pgm")
+interpolator = pygeodesy.GeoidKarney("./egm84-30.pgm", kind=2)
 
-for lat1 in range(-90, 91):
+# open an output file named geo.csv
+f = open("geo.csv", "w")
+
+for lat1 in tqdm(range(-90, 91), desc="Latitude"):
     # 1 degree resolution
     for lon1 in range(-180, 181):
         lon = lon1 / 1.0
@@ -16,6 +20,9 @@ for lat1 in range(-90, 91):
         single_position = LatLon(lat, lon)
         h = interpolator(single_position)
         result = calculator.calculate(latitude=lat, longitude=lon)
-        # store CSV, use > geo.csv to save to file
-        print(str(lat1) + "," + str(lon1) + "," + str(round(h)) + "," +
-              str(round(result['field-value']['declination']['value'])) + ",")
+        # store CSV
+        output = (str(lat1) + "," + str(lon1) + "," + str(round(h)) + "," + str(round(result['field-value']
+                                                                                      ['declination']['value'])))
+        f.write(output + "\n")
+
+f.close()
